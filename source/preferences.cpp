4fd0408d5315428940728a3aa41b9c585a8f98a4
DESKTOP-OMA6DMK\R-Sam-BB <duxiuxing@foxmail.com>
2022-01-11 00:30:23 +0800

    1. copy all .ttf files to apps/fceugx 2. the global variable appPath should be argv[0] strip file name whatever ChangeInterface() return true or false 3. set default language as english 4. replace sd1:/ with sd:/

--- source/preferences.cpp
+++ source/preferences.cpp
@@ -404,7 +404,7 @@ void FixInvalidSettings()
 	if(!(GCSettings.SFXVolume >= 0 && GCSettings.SFXVolume <= 100))
 		GCSettings.SFXVolume = 40;
 	if(GCSettings.language < 0 || GCSettings.language >= LANG_LENGTH)
-		GCSettings.language = LANG_ENGLISH;
+		GCSettings.language = LANG_DEFAULT;
 	if(GCSettings.Controller > CTRL_PAD4 || GCSettings.Controller < CTRL_MOUSE)
 		GCSettings.Controller = CTRL_PAD2;
 	if(!(GCSettings.render >= 0 && GCSettings.render < 5))
@@ -469,7 +469,7 @@ DefaultSettings ()
 	if(GCSettings.language == LANG_TRAD_CHINESE)
 		GCSettings.language = LANG_SIMP_CHINESE;
 #else
-	GCSettings.language = LANG_ENGLISH;
+	GCSettings.language = LANG_DEFAULT;
 #endif
 
 	/****************** SNES9x Settings ***********************/
@@ -633,8 +633,8 @@ LoadPrefsFromMethod (char * path)
 	{
 		strcpy(prefpath, path);
 
-		if(appPath[0] == 0)
-			strcpy(appPath, prefpath);
+		// if(appPath[0] == 0)
+		//	strcpy(appPath, prefpath);
 	}
 
 	return retval;
