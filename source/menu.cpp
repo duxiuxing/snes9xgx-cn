44c27ace5e7126671968e7f75ff0c991093f48cb
R-Sam-1980 <duxiuxing@gmail.com>
2026-04-06 03:53:19 +0800

    XBOX360: connected

--- source/menu.cpp
+++ source/menu.cpp
@@ -926,7 +926,7 @@ static void WindowCredits(void * ptr)
 
 	txt[i] = new GuiText(controllerInfo, 14, (GXColor){0, 0, 0, 255});
 	txt[i]->SetAlignment(ALIGN_LEFT, ALIGN_BOTTOM);
-	txt[i]->SetPosition(10,-60); i++;
+	txt[i]->SetPosition(20,-44); i++;
 	txt[i] = new GuiText(wiiDetails, 14, (GXColor){0, 0, 0, 255});
 	txt[i]->SetAlignment(ALIGN_RIGHT, ALIGN_BOTTOM);
 	txt[i]->SetPosition(-20,-44); i++;
