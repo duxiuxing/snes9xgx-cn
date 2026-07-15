63c3f6034fb8f48c089bafbbf24fcc902b51336e
R-Sam-1980 <duxiuxing@gmail.com>
2026-07-15 22:43:28 +0800

    code review

--- source/menu.cpp
+++ source/menu.cpp
@@ -889,39 +889,39 @@ static void WindowCredits(void * ptr)
 
 	GuiText::SetPresets(20, (GXColor){0, 0, 0, 255}, 0, FTGX_JUSTIFY_LEFT | FTGX_ALIGN_TOP, ALIGN_LEFT, ALIGN_TOP);
 	txt[i] = new GuiText("Coding & menu design");
-	txt[i]->SetPosition(60,y); i++;
+	txt[i]->SetPosition(x1,y); i++;
 	txt[i] = new GuiText("Tantric");
-	txt[i]->SetPosition(350,y); i++; y+=24;
+	txt[i]->SetPosition(x2,y); i++; y+=24;
 	txt[i] = new GuiText("Additional improvements");
-	txt[i]->SetPosition(60,y); i++;
+	txt[i]->SetPosition(x1,y); i++;
 	txt[i] = new GuiText("Zopenko, michniewski");
-	txt[i]->SetPosition(350,y); i++; y+=24;
+	txt[i]->SetPosition(x2,y); i++; y+=24;
 	txt[i] = new GuiText("InfiniteBlue, others");
-	txt[i]->SetPosition(350,y); i++; y+=24;
+	txt[i]->SetPosition(x2,y); i++; y+=24;
 	txt[i] = new GuiText("Menu artwork");
-	txt[i]->SetPosition(60,y); i++;
+	txt[i]->SetPosition(x1,y); i++;
 	txt[i] = new GuiText("the3seashells");
-	txt[i]->SetPosition(350,y); i++; y+=24;
+	txt[i]->SetPosition(x2,y); i++; y+=24;
 	txt[i] = new GuiText("Menu sound");
-	txt[i]->SetPosition(60,y); i++;
+	txt[i]->SetPosition(x1,y); i++;
 	txt[i] = new GuiText("Peter de Man");
-	txt[i]->SetPosition(350,y); i++; y+=48;
+	txt[i]->SetPosition(x2,y); i++; y+=48;
 
 	txt[i] = new GuiText("Snes9x GX GameCube");
-	txt[i]->SetPosition(60,y); i++;
+	txt[i]->SetPosition(x1,y); i++;
 	txt[i] = new GuiText("SoftDev, crunchy2,");
-	txt[i]->SetPosition(350,y); i++; y+=24;
+	txt[i]->SetPosition(x2,y); i++; y+=24;
 	txt[i] = new GuiText("eke-eke, others");
-	txt[i]->SetPosition(350,y); i++; y+=24;
+	txt[i]->SetPosition(x2,y); i++; y+=24;
 	txt[i] = new GuiText("Snes9x");
-	txt[i]->SetPosition(60,y); i++;
+	txt[i]->SetPosition(x1,y); i++;
 	txt[i] = new GuiText("Snes9x Team");
-	txt[i]->SetPosition(350,y); i++; y+=24;
+	txt[i]->SetPosition(x2,y); i++; y+=24;
 
 	txt[i] = new GuiText("libogc / devkitPPC");
-	txt[i]->SetPosition(60,y); i++;
+	txt[i]->SetPosition(x1,y); i++;
 	txt[i] = new GuiText("shagkur & WinterMute");
-	txt[i]->SetPosition(350,y); i++; y+=24;
+	txt[i]->SetPosition(x2,y); i++; y+=24;
 
 	char consoleDetails[40];
 	char memoryFreeInfo[50];
